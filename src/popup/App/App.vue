5b478915d0da44f294ecb1e3d3677cdb88eb2df1
tdreid <trevor.d.reid@gmail.com>
2019-02-27 01:54:10 +0000

    Apply base styles

--- src/popup/App/App.vue
+++ src/popup/App/App.vue
@@ -11,12 +11,17 @@ export default {
 </script>
 
 <style>
-.main_app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  @import url('../../css/normalize.css');
+  @import url('../../css/skeleton.css');
+  .main_app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    height: 240px;
+    width: 120px;
+    margin-top: 12px;
+    padding-right: 24px;
+  }
 </style>
